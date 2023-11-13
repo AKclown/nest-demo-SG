2d4ad257f5c11cb864c9e39cb7a61a532d6a0762
zhongwenkai <13413629530@139.com>
2023-11-13 19:58:53 +0800

    feat: docker compose

---
+++ packages/docker-compose-test/src/app.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Aaa } from './aaa.entity';
+import { createClient } from 'redis';
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-container',
+      port: 3306,
+      username: 'root',
+      password: 'akclown',
+      database: 'aaa',
+      synchronize: true,
+      logging: true,
+      entities: [Aaa],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'REDIS_CLIENT',
+      async useFactory() {
+        const client = createClient({
+          socket: {
+            host: 'redis-container',
+            port: 6379,
+          },
+        });
+        await client.connect();
+        return client;
+      },
+    },
+  ],
+})
+export class AppModule {}

947566f906aade319eae0f377ba46132de866d8f
zhongwenkai <13413629530@139.com>
2023-12-04 18:33:13 +0800

    feat: add cache-manager

---
+++ packages/cache-manager-test/src/my-cache.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { RedisClientType } from 'redis';
+import { of, tap } from 'rxjs';
+
+@Injectable()
+export class MyCacheInterceptor implements NestInterceptor {
+  @Inject('REDIS_CLIENT')
+  private redisClient: RedisClientType;
+
+  @Inject(HttpAdapterHost)
+  private httpAdapterHost: HttpAdapterHost;
+
+  async intercept(context: ExecutionContext, next: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+    // HttpAdapterHost 前面没用过。我们知道 Nest 底层可以切换 express、fastify 等库，而这些库都会实现一个通用的适配器，就是 HttpAdapter。
+    const key = this.httpAdapterHost.httpAdapter.getRequestUrl(request);
+
+    const value = await this.redisClient.get(key);
+    if (!value) {
+      return next.handle().pipe(
+        tap((res) => {
+          this.redisClient.set(key, res);
+        }),
+      );
+    } else {
+      return of(value);
+    }
+  }
+}
